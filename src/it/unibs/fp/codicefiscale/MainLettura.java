0e187257c233d84b213d5c5fd06351972dcde161
Paolo <paolo.biolghini@gmail.com>
2021-04-26 18:50:43 +0200

    Main lettura

---
+++ src/it/unibs/fp/codicefiscale/MainLettura.java
@@ -0,0 +1,36 @@
+package it.unibs.fp.codicefiscale;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class MainLettura {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+			DocumentBuilder Builder=factory.newDocumentBuilder();
+			Document doc=Builder.parse("Persone.xml");
+			
+			NodeList list=doc.getElementsByTagName("persona");
+			
+			for(int i=0;i<list.getLength();i++)
+			{
+				Node n=list.item(i);
+				System.out.println(n.getNodeName());
+				Element e=(Element)n;
+				System.out.println("nome:"+e.getElementsByTagName("nome").item(0).getTextContent());
+			}
+		}
+
+		catch(Exception e) {
+			System.out.println("something doesn't work");
+		}
+	}
+
+}
