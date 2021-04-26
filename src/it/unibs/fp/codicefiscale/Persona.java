06e68825a0c3d9a64130d47a8d9ff54d9ac4ae65
Paolo <paolo.biolghini@gmail.com>
2021-04-26 18:33:31 +0200

    Data

---
+++ src/it/unibs/fp/codicefiscale/Persona.java
@@ -0,0 +1,13 @@
+package it.unibs.fp.codicefiscale;
+
+public class Persona {
+
+	private String nome;
+	private String cognome;
+	private String codicefiscale;
+	private String codcomune;
+	private String sesso;
+	//class data di nascita
+	
+	
+}
