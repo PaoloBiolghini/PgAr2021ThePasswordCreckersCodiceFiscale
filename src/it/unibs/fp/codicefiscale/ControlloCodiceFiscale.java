5ff1a858e7d70d4f13c362c96bc21db0eec28ce9
PaoloBiolghini <61747117+PaoloBiolghini@users.noreply.github.com>
2021-04-27 21:26:00 +0200

    Update ControlloCodiceFiscale.java

--- src/it/unibs/fp/codicefiscale/ControlloCodiceFiscale.java
+++ src/it/unibs/fp/codicefiscale/ControlloCodiceFiscale.java
@@ -8,15 +8,15 @@ public class ControlloCodiceFiscale {
 
 	
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(controllaCodice("FSTPLA98M01B157E"));
-	}
+	}*/
 	
 	
 	//metodo invocato per controllare il codice fiscale
-	//ritorna true se il codice è corretto
-	//ritorna false se è presente un qualche tipo di errore
+	//ritorna true se il codice Ã¨ corretto
+	//ritorna false se Ã¨ presente un qualche tipo di errore
 	public static boolean controllaCodice(String codice)
 	{
 if(codice.length()!=16) return false;
@@ -45,7 +45,7 @@ if(codice.length()!=16) return false;
 	}
 	
 	//controlla che siano composte solamente da lettere
-	//se è presente almeno un simbolo diverso da lettere ritorna false
+	//se Ã¨ presente almeno un simbolo diverso da lettere ritorna false
 	public static boolean controlloLettere(String lettere){
 		for(int i=0;i<lettere.length();i++){
 			if(!Character.isLetter(lettere.charAt(i)))
@@ -57,8 +57,8 @@ if(codice.length()!=16) return false;
 		return true;
 	}
 	
-	//metodo che riceve una stringa e controlla se questa è composta solamente
-	//da numeri e in questo caso ritorna true e se invece è composta anche da almeno un carattere
+	//metodo che riceve una stringa e controlla se questa Ã¨ composta solamente
+	//da numeri e in questo caso ritorna true e se invece Ã¨ composta anche da almeno un carattere
 	//diverso da un numero ritorna false
 	public static boolean controlloNumeri(String numeri){
 		for(int i=0;i<numeri.length();i++)
@@ -74,8 +74,8 @@ if(codice.length()!=16) return false;
 
 	//metodo che prende in input la porzione di codice fisclae mese e giorno
 	//selezione il massimo dei giorni in base al mese e controlla
-	//ritorna false se il giorno è maggiore del massimo giorno possibile
-	//ritorno true se è inferiore o uguale al massimo
+	//ritorna false se il giorno Ã¨ maggiore del massimo giorno possibile
+	//ritorno true se Ã¨ inferiore o uguale al massimo
 	public static boolean controlloGiorno(String mese,int giorno)
 	{
 		List<String> codMese=Arrays.asList("A","B","C","D","E","H","L","M","P","R","S","T");
@@ -94,8 +94,8 @@ if(codice.length()!=16) return false;
 	}
 	
 	//metodo che passa al metodo della classe comune il codice comune
-	//ritorna true se il codice è stato trovato
-	//ritorna false se non è trovato il codice comune
+	//ritorna true se il codice Ã¨ stato trovato
+	//ritorna false se non Ã¨ trovato il codice comune
 	public static boolean controlloComune(String codice) {
 		
 		//chiamare classe comuni dove passo il codice del comune e il codice provincia
