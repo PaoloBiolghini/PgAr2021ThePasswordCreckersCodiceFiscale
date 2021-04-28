8d9f92305e1131704b8cabaf2b8e757a383abf76
Kevin-L-Github <k.liu@studenti.unibs.it>
2021-04-28 19:44:26 +0200

    i

--- src/it/unibs/fp/codicefiscale/Data.java
+++ src/it/unibs/fp/codicefiscale/Data.java
@@ -25,7 +25,12 @@ public class Data {
 	public int getYear() {
 		return year;
 	}
-
+	
+	public int getLastTwoDigitsYear()
+	{
+		return this.year % 100 ;
+	}
+	
 	public void setDay(int day) {
 		this.day = day;
 	}
@@ -73,6 +78,25 @@ public class Data {
 		
 		Data birthday = new Data (giorno, mese , anno);
 		return birthday;
+	}
+	
+	public static String mese (int mese) {
+		switch (mese)
+		{
+		case 1 : return "A" ;
+		case 2 : return "B" ;
+		case 3 : return "C" ;
+		case 4 : return "D" ;
+		case 5 : return "E" ;
+		case 6 : return "H" ;
+		case 7 : return "L" ;
+		case 8 : return "M" ;
+		case 9 : return "P" ;
+		case 10 : return "R" ;
+		case 11 : return "S" ;
+		case 12 : return "T" ;
+		default : return "";
+		}
 	}
 	
 }
