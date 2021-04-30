b51ac9aea5a0d3391dc6d0db26e19a2c57f6bc0b
Kevin-L-Github <k.liu@studenti.unibs.it>
2021-04-30 17:30:44 +0200

    Prova

--- src/it/unibs/fp/codicefiscale/Data.java
+++ src/it/unibs/fp/codicefiscale/Data.java
@@ -4,15 +4,15 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Data {
-	
-	private int day ;
-	private int month ;
-	private int year ;
-	
-	public Data(int _day , int _month , int _year) {
-		this.day = _day ;
-		this.month = _month ;
-		this.year = _year ;
+
+	private int day;
+	private int month;
+	private int year;
+
+	public Data(int _day, int _month, int _year) {
+		this.day = _day;
+		this.month = _month;
+		this.year = _year;
 	}
 
 	public int getDay() {
@@ -26,12 +26,11 @@ public class Data {
 	public int getYear() {
 		return year;
 	}
-	
-	public int getLastTwoDigitsYear()
-	{
-		return this.year % 100 ;
+
+	public int getLastTwoDigitsYear() {
+		return this.year % 100;
 	}
-	
+
 	public void setDay(int day) {
 		this.day = day;
 	}
@@ -43,55 +42,72 @@ public class Data {
 	public void setYear(int year) {
 		this.year = year;
 	}
-	
+
 	/**
-	 * estrae da una data stringa (yyyy-MM-dd) i singoli valori e restituisce un oggetto di tipo Data
+	 * estrae da una data stringa (yyyy-MM-dd) i singoli valori e restituisce un
+	 * oggetto di tipo Data
 	 * 
 	 * @param data
 	 * @return Data
 	 */
-	public static Data estraiData (String data){
+	public static Data estraiData(String data) {
 
-		int anno = 0 , mese = 0 , giorno = 0 ;
+		int anno = 0, mese = 0, giorno = 0;
 		anno = Integer.parseInt(data.substring(0, 4));
-		mese =  Integer.parseInt(data.substring(5, 7));
+		mese = Integer.parseInt(data.substring(5, 7));
 		giorno = Integer.parseInt(data.substring(8, 10));
-		Data birthday = new Data (giorno, mese , anno);
+		Data birthday = new Data(giorno, mese, anno);
 		return birthday;
 	}
+
 	/**
-	 * Restituisce il Carattere che rappresenta il mese nell'ambito del codice fiscale
+	 * Restituisce il Carattere che rappresenta il mese nell'ambito del codice
+	 * fiscale
+	 * 
 	 * @param mese
 	 * @return
 	 */
-	public static String mese (int mese) {
-		switch (mese)
-		{
-		case 1 : return "A" ;
-		case 2 : return "B" ;
-		case 3 : return "C" ;
-		case 4 : return "D" ;
-		case 5 : return "E" ;
-		case 6 : return "H" ;
-		case 7 : return "L" ;
-		case 8 : return "M" ;
-		case 9 : return "P" ;
-		case 10 : return "R" ;
-		case 11 : return "S" ;
-		case 12 : return "T" ;
-		default : return "";
+	public static String mese(int mese) {
+		switch (mese) {
+		case 1:
+			return "A"; 
+		case 2:
+			return "B";
+		case 3:
+			return "C";
+		case 4:
+			return "D";
+		case 5:
+			return "E";
+		case 6:
+			return "H";
+		case 7:
+			return "L";
+		case 8:
+			return "M";
+		case 9:
+			return "P";
+		case 10:
+			return "R";
+		case 11:
+			return "S";
+		case 12:
+			return "T";
+		default:
+			return "";
 		}
 	}
+
 	/**
 	 * formatta i numeri in un formato che possiede due cifre
+	 * 
 	 * @param giorno
 	 * @return String
 	 */
-	public static String formatta(int giorno)
-	{
+	public static String formatta(int giorno) {
 		DecimalFormat formatter = new DecimalFormat("00");
 		String formattato = formatter.format(giorno);
 		return formattato;
 	}
-	
+
 }
--- src/it/unibs/fp/codicefiscale/Persona.java
+++ src/it/unibs/fp/codicefiscale/Persona.java
@@ -9,60 +9,63 @@ public class Persona {
 	private String sesso1;
 	private Data compleanno;
 
-	
 	public Persona(String nome, String cognome, String comune1, String sesso, String compleanno) {
 		super();
 		this.nome = nome;
 		this.cognome = cognome;
 		this.comune1 = comune1;
 		this.sesso1 = sesso;
-		this.compleanno = Data.estraiData(compleanno) ;
+		this.compleanno = Data.estraiData(compleanno);
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public String getCognome() {
 		return cognome;
 	}
+
 	public String getCodiceFiscale() {
 		return codiceFiscale;
 	}
+
 	public String getComune() {
 		return comune1;
 	}
+
 	public String getSesso() {
 		return sesso1;
 	}
-	
+
 	public String getCompleanno() {
-		String data=compleanno.getDay()+"-"+compleanno.getMonth()+"-"+compleanno.getYear();
-		return data; 
+		String data = compleanno.getDay() + "-" + compleanno.getMonth() + "-" + compleanno.getYear();
+		return data;
 	}
 
 //chiedere se è buona cosa mettere questo metodo in persona o farlo nel main, chiedere per la classe comune come faccio a prenderlo,e per la data,
-	//per la differenza nel utilizzo tra string e string buffer
+	// per la differenza nel utilizzo tra string e string buffer
 	public StringBuffer codice(String nome, String cognome, char sesso, Comune comune) {
 		StringBuffer codiceFiscale = new StringBuffer();
-		//prime 3 lettere del cognome
+		// prime 3 lettere del cognome
 		if (cognome.length() < 3) {
 			codiceFiscale.append(cognome);
 			codiceFiscale.append("X");
-		} else codiceFiscale.append(lastNametLetters(cognome));
+		} else
+			codiceFiscale.append(lastNametLetters(cognome));
 
-		//lettere nome
+		// lettere nome
 
-		if(nome.length()<3){
+		if (nome.length() < 3) {
 			codiceFiscale.append(nome);
 			codiceFiscale.append("X");
-		}else codiceFiscale.append(nameLetters(nome));
-
-
+		} else
+			codiceFiscale.append(nameLetters(nome));
 
 		return codiceFiscale;
 	}
 
-	private StringBuffer consonant(String checkCon){ //restituisce una stringa contenente le consonanti
+	private StringBuffer consonant(String checkCon) { // restituisce una stringa contenente le consonanti
 		StringBuffer cons = new StringBuffer();
 
 		for (int i = 0; i < checkCon.length(); i++)
@@ -72,65 +75,62 @@ public class Persona {
 		return cons;
 	}
 
-private StringBuffer vowel(String checkVow){ //restituisce una stringa contenente le vocali
-	StringBuffer vow = new StringBuffer();
-	for (int i = 0; i < checkVow.length(); i++)
-		if (!isConsonant(checkVow.charAt(i))) {
-			vow.append(checkVow.charAt(i));
-		}
+	private StringBuffer vowel(String checkVow) { // restituisce una stringa contenente le vocali
+		StringBuffer vow = new StringBuffer();
+		for (int i = 0; i < checkVow.length(); i++)
+			if (!isConsonant(checkVow.charAt(i))) {
+				vow.append(checkVow.charAt(i));
+			}
 		return vow;
-}
-
+	}
 
 	private StringBuffer lastNametLetters(String check) {
 		StringBuffer lett = new StringBuffer(3);
 
-		StringBuffer lastNameCons=new StringBuffer(consonant(check));
+		StringBuffer lastNameCons = new StringBuffer(consonant(check));
 		lett.append(lastNameCons);
-		if(lastNameCons.length()<3){
-			StringBuffer lastNameVow=new StringBuffer(vowel(check));
-			int i=0;
-			while (lett.length()<3){
+		if (lastNameCons.length() < 3) {
+			StringBuffer lastNameVow = new StringBuffer(vowel(check));
+			int i = 0;
+			while (lett.length() < 3) {
 				lett.append(lastNameVow.charAt(i));
 				i++;
 			}
 		}
 
-			return lett;
-
+		return lett;
 
 	}
 
-	private StringBuffer nameLetters(String check){
+	private StringBuffer nameLetters(String check) {
 		StringBuffer lett = new StringBuffer(3);
 
-		StringBuffer nameCons=new StringBuffer(consonant(check));
-		if(nameCons.length()>=4){
+		StringBuffer nameCons = new StringBuffer(consonant(check));
+		if (nameCons.length() >= 4) {
 			lett.append(nameCons.charAt(0));
 			lett.append(nameCons.charAt(2));
 			lett.append(nameCons.charAt(3));
-		}else if(nameCons.length()==3){
-			for (int i=0;i<3;i++)
-			lett.append(nameCons.charAt(i));
+		} else if (nameCons.length() == 3) {
+			for (int i = 0; i < 3; i++)
+				lett.append(nameCons.charAt(i));
 
-		}else if(nameCons.length()<2){
-			StringBuffer nameVow=new StringBuffer(vowel(check));
-			int i=0;
-			while (lett.length()<3){
+		} else if (nameCons.length() < 2) {
+			StringBuffer nameVow = new StringBuffer(vowel(check));
+			int i = 0;
+			while (lett.length() < 3) {
 				lett.append(nameVow.charAt(i));
 				i++;
 			}
 		}
 
-return lett;
-
+		return lett;
 
 	}
 
-	private int numConsonant(String check){
-		int cont=0;
-		for(int i=0;i<check.length();i++){
-			if(isConsonant(check.charAt(i)))
+	private int numConsonant(String check) {
+		int cont = 0;
+		for (int i = 0; i < check.length(); i++) {
+			if (isConsonant(check.charAt(i)))
 				cont++;
 		}
 
